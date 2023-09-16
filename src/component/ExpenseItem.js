c0290679a0acbb76e9115a636b726f4f9b23e034
varshagm119 <varshagm96@gmail.com>
2023-09-16 21:23:28 +0530

    initial commit

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Item</h2>
+      <div>
+        Food Rs10
+      </div>
+      <div>
+        petrol Rs100
+      </div>
+      <div>
+        movie Rs200
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
