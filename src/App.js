f5457a5fca1bf8339c78fd8c6aeef4f63799ea1b
varshagm119 <varshagm96@gmail.com>
2023-09-18 20:05:50 +0530

    the new code arranged with the udemy trainer

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import ExpenseItem from "./component/ExpenseItem";
-const App = () => {
+import React from 'react';
 
+import Expenses from './component/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      LocationOfExpenditure: 'Bengaluru'
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12),LocationOfExpenditure:'Tumkuru' },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      LocationOfExpenditure:'Mysuru'
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
-      LocationOfExpenditure:'Delhi'
     },
   ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} LocationOfExpenditure={expenses[0].LocationOfExpenditure} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} LocationOfExpenditure={expenses[1].LocationOfExpenditure} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} LocationOfExpenditure={expenses[2].LocationOfExpenditure} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} LocationOfExpenditure={expenses[3].LocationOfExpenditure} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
-
+      <Expenses items={expenses} />
     </div>
   );
 }
