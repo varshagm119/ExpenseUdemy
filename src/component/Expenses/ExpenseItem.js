eecf2641bde87e24603146b934ac288d11ebc486
varshagm119 <varshagm96@gmail.com>
2023-09-20 15:19:54 +0530

    changed amt to 100

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,21 +1,31 @@
 import React from 'react';
-
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [title,setTitle] = useState(props.title);
+ 
+  const[amt,setAmt] = useState(props.amount);
+  const amtHandle = () => {
+    setAmt('100');
+    //document.getElementById('amtBt').innerHTML='hi'
+    console.log(amt)
+  }
   const deleteExpenseHandler = () =>{
-    console.log('clicked')
+    setTitle('Updated!');
+    console.log(0);
   }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price' id='amtBt'>${amt}</div>
       </div>
-      <button onClick={deleteExpenseHandler}>Delete</button>
+      <button onClick={deleteExpenseHandler}>Change title</button>
+      <button onClick={amtHandle}>resetAmt</button>
     </Card>
   );
 }
