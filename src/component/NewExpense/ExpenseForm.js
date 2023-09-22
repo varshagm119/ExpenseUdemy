1cc633457742b042ab1e21c456a5946b61aac961
varshagm119 <varshagm96@gmail.com>
2023-09-22 16:08:22 +0530

    added states for all 3 input values

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -1,35 +1,67 @@
-import React from "react";
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const titleChangeHandle = (event) =>{
-        console.log(event.target.value);
-    }
-    const amtChangeHandle = (event) =>{
-        console.log(event.target.value);
-    }
-    const dateChangeHandle = (event) =>{
-        console.log(event.target.value);
-    }
-    return (<form>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type="text" onChange={titleChangeHandle}/>
-            </div>
-            <div className="new-expense__control">
-                <label>Amount</label>
-                <input type="number" min="0.1" step="0.1" onChange={amtChangeHandle} />
-            </div>
-            <div className="new-expense__control">
-                <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandle}/>
-            </div>
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmt, setEnteredAmt] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+//using a single state instea:-
+// const [userInput,setUserInput] = useState({
+//     enteredTitle: '',
+//     enteredAmt: '',
+//     enteredDate: ''
+// })
+  const titleChangeHandle = (event) => {
+    setEnteredTitle(event.target.value);
+    //the follwing is used when a single state is used for all the input values:-
+    //setUserInput({...userInput,enteredTitle: event.target.value});
+    //enteredTitle is override after the spread operator
+    //the best way to write the above func that keeps the previous state:-
+    // setUserInput((prevState) => {
+    //     return {...prevState,enteredTitle: event.target.value}
+    // })
+
+  };
+  const amtChangeHandle = (event) => {
+    setEnteredAmt(event.target.value);
+    //setUserInput({...userInput,enteredAmt: event.target.value});
+  };
+  const dateChangeHandle = (event) => {
+    setEnteredDate(event.target.value);
+    //setUserInput({...userInput,enteredDate: event.target.value});
+  };
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandle} />
         </div>
-        <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.1"
+            step="0.1"
+            onChange={amtChangeHandle}
+          />
         </div>
-    </form>);
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandle}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
